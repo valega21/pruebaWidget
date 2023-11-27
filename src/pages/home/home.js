5ce307c67ae95ed681dcf1fc9a6c9c6b9d6c7431
valega21 <valega21@gmail.com>
2023-11-27 17:47:07 -0600

    cambios homePage parametros por url

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -5,11 +5,20 @@ import Medallero from "components/medallero/medallero";
 
 export default function HomePage({type, longitud}) {
 
+    const url = window.location.href;
+    const parametros = new URLSearchParams(new URL(url).search);//
+
+    // Obten valores específicos de los parámetros
+    const valorParametro1 = parametros.get('longitud');
+    const valorParametro2 = parametros.get('type');
+
+    console.log(" parametro url... ", valorParametro1)
+
     return (
         <div className="principal">
 
 
-            <Medallero longitud={10}/>
+            <Medallero longitud={valorParametro1}/>
             
         </div>
     )
